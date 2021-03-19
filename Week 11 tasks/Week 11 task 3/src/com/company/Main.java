7d6b608a46d505b0c4ed82a11d6fc20f4281b321
viggo23111 <vbnymand@gmail.com>
2021-03-19 09:55:49 +0100

    Week 11 task update

--- Week 11 tasks/Week 11 task 3/src/com/company/Main.java
+++ Week 11 tasks/Week 11 task 3/src/com/company/Main.java
@@ -22,25 +22,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Account account1;
-        Account account2;
-        Account account3;
-        Account account4;
-        account1=new Account( "1/3/09 14:44","Product1",1200,"Visa","4593781145107322","Gouya","Echuca","Victoria","Australia","9/25/05 21:13","1/3/09 14:22",-36.1333333,144.75);
-        System.out.println(account1.getCard_Number());
+        Account[] accounts = new Account[4];
+        //assign value to array
+        accounts[0] = new Account( "1/3/09 14:44","Product1",1200,"Visa","4593781145107322","Gouya","Echuca","Victoria","Australia","9/25/05 21:13","1/3/09 14:22",-36.1333333,144.75);
+        accounts[1] = new Account( "1/5/09 11:37","Product1",1200,"Visa","4234120954489197","Janet","Ottawa","Ontario","Canada","1/5/09 9:35","1/5/09 19:24",-27.1666667,152.95);
+        accounts[2] = new Account( "1/5/09 20:00","Product2",3600,"Visa","4904344348439820","James","Burpengary","Queensland","Australia","12/10/08 19:53","1/8/09 17:58",45.4166667,-75.7);
+        accounts[3] = new Account( "1/3/09 13:24","Product1",1200,"Visa","4737470823565213","Mehmet Fatih","Helsingor","Frederiksborg","Denmark","1/3/09 12:47","1/9/09 11:14",56.0333333,12.6166667);
 
-        account2=new Account( "1/5/09 11:37","Product1",1200,"Visa","4234120954489197","Janet","Ottawa","Ontario","Canada","1/5/09 9:35","1/5/09 19:24",-27.1666667,152.95);
-        System.out.println(account2.getCard_Number());
-
-        account3=new Account( "1/5/09 20:00","Product2",3600,"Visa","4904344348439820","James","Burpengary","Queensland","Australia","12/10/08 19:53","1/8/09 17:58",45.4166667,-75.7);
-        System.out.println(account3.getCard_Number());
-
-        account4=new Account( "1/3/09 13:24","Product1",1200,"Visa","4737470823565213","Mehmet Fatih","Helsingor","Frederiksborg","Denmark","1/3/09 12:47","1/9/09 11:14",56.0333333,12.6166667);
-        System.out.println(account4.getCard_Number());
-
-        account1.setCard_Number("4593781145107322");
-        System.out.println(account1.getCard_Number());
-        System.out.println(account1);
+        accounts[0].setCard_Number("4593781145107322");
+        System.out.println(accounts[0].getCard_Number());
+        System.out.println(accounts[0]);
     }
 }
